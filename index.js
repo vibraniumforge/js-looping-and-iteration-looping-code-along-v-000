ecd76393f39e8f0f3328ee92f1cd2a2ace61b714
Greg Morgan <gregm9999@gmail.com>
2019-01-09 01:35:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,11 +8,10 @@ function printBadges(names) {
 
 
 function tailsNeverFails() {
-  let number=Math.random();
   let counter=0;
   console.log(number);
   console.log(counter);
-  while ( number < 0.5) {
+  while ( Math.random() < 0.5) {
     counter++;
   }
   console.log("`You got ${counter} tails in a row!`");
