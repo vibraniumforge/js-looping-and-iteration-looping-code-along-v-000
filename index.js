dee12df90a7959ecfdb54443e2056c0cb0d928f3
Greg Morgan <gregm9999@gmail.com>
2019-01-09 01:16:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Code your solutions in this file
+function printBadges(names) {
+  for (let i=0; i<names.length;i++) {
+    console.log("Welcome", names[i],"! You are employee number ", i+1)
+  }
+}
