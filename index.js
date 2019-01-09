164273cf44aa5460de7483a1ad278e507d0c82ad
Greg Morgan <gregm9999@gmail.com>
2019-01-09 01:42:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function printBadges(names) {
+  for (let i=0; i < names.length; i++) {
+    console.log(`Welcome ${names[i]}! You are employee #${i+1}.`)
+  }
+  return names;
+}
+
+
+function tailsNeverFails() {
+  function random() {
+    return Math.random() >=0.5;
+  }
+  let counter=0;
+  while (random()) {
+    counter++;
+  }
+  return `You got ${counter} tails in a row!`;
+}
