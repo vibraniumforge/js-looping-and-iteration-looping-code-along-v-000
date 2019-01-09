5bc2c3633399a7ef7323d456782cc95a2ed12665
Greg Morgan <gregm9999@gmail.com>
2019-01-09 01:39:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,5 +15,5 @@ function tailsNeverFails() {
   while (random()) {
     counter++;
   }
-  console.log("`You got ${counter} tails in a row!`");
+  console.log(`You got ${counter} tails in a row!`);
 }
