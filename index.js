302b5eb007f7a44ac4c7f0d58b09163d601add67
Greg Morgan <gregm9999@gmail.com>
2019-01-09 01:28:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-// Code your solutions in this file
-function printBadges(names) {
-  for (let i=0; i < names.length; i++) {
-    console.log(`Welcome ${names[i]}! You are employee #${i+1}.`)
-  }
-  return names;
-}
+// // Code your solutions in this file
+// function printBadges(names) {
+//   for (let i=0; i < names.length; i++) {
+//     console.log(`Welcome ${names[i]}! You are employee #${i+1}.`)
+//   }
+//   return names;
+// }
 
 
 function tailsNeverFails() {
